34c22f071c67f265da09ec04f6472317fcdc30d6
wallyz <wallyzielinski@gmail.com>
2025-02-04 19:10:37 -0500

    scaffolding and setup

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import "./scss/globals.scss";
+
+export const metadata = {
+  title: "RESIST",
+  description: "Fight back.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,56 @@
+
+export default function Home() {
+  return (
+    <>
+      <main id='main'>
+        <h1>RESIST.</h1>
+
+        <p>To spread knowledge, awareness, and tools which enable everyday American citizens to actively fight back against fascist regimes and the oligarchy, RESIST uses word of mouth and other analog techniques to connect and unite people in their community.</p>
+
+        <h2>I'm just one person. How do I resist?</h2>
+        <ol>
+          <li>
+            Divest from corporations.
+            <ul>
+              <li>Cease the transfer of wealth from the working class to the oligarchs</li>
+              <li>Unsubscribe from Amazon Prime</li>
+              <li>Cancel your subscriptions</li>
+              <li>Spend money locally at small businesses</li>
+            </ul>
+          </li>
+          <li>
+            Reclaim your attention.
+            <ul>
+              <li>Stop scrolling</li>
+              <li>TikTok, Instagram, and other "just keep scrolling" apps are putting you in the passenger seat of the content you consume</li>
+              <li>The more you make intention choices about your media consumption, the less control they have over you</li>
+            </ul>
+          </li>
+          <li>
+            Show up for your community.
+            <ul>
+              <li>When they come for your neighbor, take action</li>
+              <li>These are the things people most associate with resistance movements, but they don't happen every day</li>
+              <li>Make friends with your neighbors, take your headphones off on the bus, and be in community with the working class</li>
+            </ul>
+          </li>
+        </ol>
+
+        <h2>Friendly Organizations in Philly.</h2>
+
+        <p>The following orgs are putting their support behind the RESIST movement and can host meetups, offer sanctuary, or otherwise support the cause.</p>
+        <ul>
+          <li></li>
+        </ul>
+
+        <h2>Mutual Aid Organizations.</h2>
+
+        <p>If you need support or if you want to volunteer to help your neighbor in need, these are the places to do so.</p>
+
+        <ul>
+          <li></li>
+        </ul>
+      </main>
+    </>
+  );
+}
